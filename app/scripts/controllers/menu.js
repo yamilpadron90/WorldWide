7e4c7ecafc39ea20f441fe519dce32e46ed29cef
Yamil <Yamil@YAMIL>
2016-09-13 12:55:05 +0400

    My test from NetBeans

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name worldWideApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the worldWideApp
+ */
+angular.module('worldWideApp')
+  .controller('MenuCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
